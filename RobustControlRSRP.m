a7a875ead113440671df9efe6540067e019f4d22
Laurens Hoogenboom <lphoogenboom@zelandlabs.com>
2022-01-17 14:00:56 +0100

    adds q2

--- RobustControlRSRP.m
+++ RobustControlRSRP.m
@@ -28,7 +28,7 @@ s = tf([1 0],1); % define s so we can easily setup TFs
 Wp = [ (0.5556*s+2.0897)/(s+2.0897E-4) 0 ; 0 .2 ];
 Wu = [ .01 0 ; 0  (5E-3*s^2+7E-4*s+5E-5)/(s^2+14E-4*s+10E-6)];
 
-Wi = [((1/16*pi)*s+0.3)/((1/64*pi)*s+1) 0 ; 0 ((1/16*pi)*s+0.3)/((1/64*pi)*s+1)];
+Wi = [((1/(16*pi))*s+0.3)/((1/(64*pi))*s+1) 0 ; 0 ((1/(16*pi))*s+0.3)/((1/(64*pi))*s+1)];
 Wo = [(0.05*s+0.2)/(0.01*s+1) 0 ; 0 (0.05*s+0.2)/(0.01*s+1)];
 
 
@@ -36,22 +36,23 @@ Wo = [(0.05*s+0.2)/(0.01*s+1) 0 ; 0 (0.05*s+0.2)/(0.01*s+1)];
 [svi,freqi] = sigma(Wi); % singular values and frequencies for input uncertainty
 [svo,freqo] = sigma(Wo); % singular values and frequencies for output uncertainty
 
-figure(1); clf; hold on; % plot singular values for uncertainties
-h1 = sigmaplot(Wi, ops);
-h2 = sigmaplot(Wo, ops);
-legend("Wi","Wo");
+% figure(1); clf; hold on; % plot singular values for uncertainties
+% h1 = sigmaplot(Wi, ops, "b--o");
+% h2 = sigmaplot(Wo, ops, "r--o");
+% title("Singular values of W_i & W_o")
+% legend("Wi","Wo");
 
-% Blockify incertainty
-H = ultidyn('H',[1,1], "Bound", 1 )
-H = [ H 0 ; 0 H ];
-
-delta =  [Wi*H zeros(2) ; zeros(2) Wo*H];
-
-figure(2); clf;
-bode(Wi(1,1)*H)
+H1 = [Wi(1,1)*ultidyn('D_1',[1,1]), 0; 0, Wi(2,2)*ultidyn('D_2',[1,1])];
+H2 = [Wo(1,1)*ultidyn('D_3',[1,1]), 0; 0, Wo(2,2)*ultidyn('D_4',[1,1])];
+delta = [H1, zeros(2); zeros(2), H2];
+nom1 = H1(1,1).NominalValue;
+figure(2); clf; hold on;
+bode(H1, "b", .5*Wi, "r")
+title("Frequency response of uncertainty W_i")
 grid on;
-
-figure(3); clf;
-bode(Wo(1,1)*H)
-grid on;
+% 
+% figure(3); clf;
+% bode(H2)
+% title("Frequency response of uncertainty W_o")
+% grid on;
 
